aea7d4d5f06a91c9e2f8e12ad0fdc0941faa7cab
jyc0922 <yuc.jin@outlook.com>
2025-07-08 20:00:41 +0900

    0708 class

---
+++ study/0708/ch01_5/src/pages/onClick.tsx
@@ -0,0 +1,13 @@
+const rootDiv = document.getElementById("root");
+if (rootDiv) {
+    rootDiv.onclick = (e: Event) => {
+        const {isTrusted, target, bubbles} = e;
+        console.log('mouse click occurs on rootDiv', isTrusted, target, bubbles);
+        
+    }
+    rootDiv.onclick = (e: Event) => {
+        const {isTrusted, target, bubbles} = e;
+        console.log('mouse click also occurs on rootDiv', isTrusted, target, bubbles);
+        
+    }
+}
