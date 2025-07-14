4330b0ead8efac349becebf40cf048b6864dd5a0
jyc0922 <yuc.jin@outlook.com>
2025-07-14 10:47:20 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.22 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -0,0 +1,22 @@
+function solution(n) {
+    var answer = 0;
+    const MOD = 1234567;
+    
+    if (n === 1) {
+        return 1;
+    } 
+    if (n ===2) {
+        return 2;
+    }
+    
+    const dp = new Array(n+1).fill(0)
+    dp[1] = 1;
+    dp[2] = 2;
+    
+    for (let i = 3; i <= n; i++) {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;}
+    
+    answer = dp[n];   
+    
+    return answer;
+}
